967c0426ec8eb82be567e8eefa30f6e7b610c76d
RedstonedLife <37072229+RedstonedLife@users.noreply.github.com>
2018-04-16 10:16:19 +0300

    Create Main.py

---
+++ Python/Discord.py Bot/Main.py
@@ -0,0 +1,10 @@
+import discord
+from discord.ext import commands
+import requests
+import random
+
+bot = commands.Bot(command_prefix="BotPrefix")
+version="0.0.1_a or a version you prefer!"
+
+@bot.ready
+async def on_ready
