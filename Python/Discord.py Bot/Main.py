02b16b53097792d99fc2a5b0de97765ff596ca45
RedstonedLife <37072229+RedstonedLife@users.noreply.github.com>
2018-04-17 10:06:01 +0300

    Update Main.py

--- Python/Discord.py Bot/Main.py
+++ Python/Discord.py Bot/Main.py
@@ -6,5 +6,14 @@ import random
 bot = commands.Bot(command_prefix="BotPrefix")
 version="0.0.1_a or a version you prefer!"
 
+@bot.command(pass_context=True)
+async def info(ctx):
+  embed=discord.Embed(title="Titles",description="Description",color=0xFFFFFF) #Color must be in hexadecimal
+  await 
+
+
 @bot.ready
-async def on_ready
+async def on_ready():
+  print("Bot is ready for usage!")
+  
+  
