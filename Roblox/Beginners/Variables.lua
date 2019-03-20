888ecc0d2f549e95d119f464c37fed4142dee9a6
RedstonedLife <37072229+RedstonedLife@users.noreply.github.com>
2019-03-20 21:41:40 +0200

    Delete Variables.lua

--- Roblox/Beginners/Variables.lua
+++
@@ -1,3 +0,0 @@
-local hi = "Hi is a string value." -- It allows us to save text forms
-local hello = 1 -- hello is a int value any number
-local ahm = true -- ahm is a bool value, either false or true
