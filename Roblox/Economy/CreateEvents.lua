f4fecb152facc0017efedb60d24369a0c597aefc
RedstonedLife <37072229+RedstonedLife@users.noreply.github.com>
2019-03-20 21:42:01 +0200

    Delete CreateEvents.lua

--- Roblox/Economy/CreateEvents.lua
+++
@@ -1,9 +0,0 @@
-script.Parent = game.ReplicatedStorage
-local rs = game:GetService("ReplicatedStorage")
-
-local newTransfer = Instance.new("RemoteEvent", rs)
-newTransfer.Name = "Transfer"
-local newEditCash = Instance.new("RemoteEvent", rs)
-newEditCash.Name = "EditCash"
-script:Destroy()
--- End of Script
